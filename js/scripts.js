4c01f33a07813748e553b055b29de312be859577
Larry <kaul.lawrence@gmail.com>
2021-03-23 19:17:05 -0500

    next test commit

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,8 @@ $(function () {
 
     $("#reserveButton").click(function(){
         $('#reserveModal').modal('show');
-    })
+    });
     $("#loginButton").click(function(){
         $('#loginModal').modal('show');
-    })
+    });
 })
